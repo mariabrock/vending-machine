a192c662d825d33d875737511590e3a3d0f99f54
mariabrock <mariabrock414@gmail.com>
2019-11-02 12:36:18 -0500

    snackpositions

--- src/javascripts/components/Machine/machine.js
+++ src/javascripts/components/Machine/machine.js
@@ -5,12 +5,6 @@ const buildTheMachine = () => {
   smash.getCompleteMachine()
     .then((singleMachine) => console.log('1 machine', singleMachine))
     .catch((error) => console.error(error));
-
-// 1. getMachines - returns first machine (hardcode)
-// 2. use machineId to get all positions for that machine
-// 3. use machineId to get all snack positions
-// 4. use uid of snackPositions/positions to get available snacks for that machine
-// 5. SMASH 'EM - return and array of positions (in order A1, A2, A3, B1....). so positions should have position.snack if a snack exists in that position
 };
 
 export default { buildTheMachine };
--- src/javascripts/helpers/data/smash.js
+++ src/javascripts/helpers/data/smash.js
@@ -1,16 +1,20 @@
 import machineData from './machineData';
 import positionData from './positionData';
+import snackPositionData from './snackPositionData';
 
 const getCompleteMachine = () => new Promise((resolve, reject) => {
-  machineData.getMachine()
-    .then((singleMachine) => positionData.getAllPositionsByMachineId(singleMachine.id))
-    .then((positions) => resolve(positions))
-    .catch((error) => reject(error));
-// 1. getMachines - returns first machine (hardcode)
+  // 1. getMachines - returns first machine (hardcode)
 // 2. use machineId to get all positions for that machine
 // 3. use machineId to get all snack positions
 // 4. use uid of snackPositions/positions to get available snacks for that machine
 // 5. SMASH 'EM - return and array of positions (in order A1, A2, A3, B1....). so positions should have position.snack if a snack exists in that position
+  machineData.getMachine()
+    .then((singleMachine) => positionData.getAllPositionsByMachineId(singleMachine.id))
+    .then((positions) => {
+      snackPositionData.getAllSnackPositionsByMachineId(positions[0].machineId)
+        .then((snackPositions) => resolve(snackPositions));
+    })
+    .catch((error) => reject(error));
 });
 
 export default { getCompleteMachine };
