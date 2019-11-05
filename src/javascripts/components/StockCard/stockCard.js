0b6264cd07ca83d8202fb057766ba7263a8bfcd6
mariabrock <mariabrock414@gmail.com>
2019-11-04 19:52:30 -0600

    delete works

--- src/javascripts/components/StockCard/stockCard.js
+++ src/javascripts/components/StockCard/stockCard.js
@@ -5,8 +5,9 @@ const makeASnack = (snack) => {
   <div class="col-3 card snack-card">
   <div class="card-body">
     <h5 class="card-title">${snack.name}</h5>
-    <p class="card-text">${snack.price}</p>
-    <p class="card-text">${snack.position.position}</p>
+    <p class="card-text">${snack.price / 100}</p>
+  </div>
+  <div class="card-footer"><button class="btn btn-danger delete-snack-position" id="${snack.snackPositionId}">Remove from ${snack.position.position}</button>
   </div>
 </div>
 `;
